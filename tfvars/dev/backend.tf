32d3c93bbec2b935f32fb33fb150b9a88a2f0528
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-26 14:32:36 +0530

    Terraform

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,5 +1,5 @@
-bucket = "remote-state-86s-dev"
-key    = "tfvars-multi-env-demo"
-region = "us-east-1"
+bucket       = "remote-state-sureshbandaru"
+key          = "tfvars-multi-env-demo"
+region       = "us-east-1"
 use_lockfile = true
-encrypt = true
+encrypt      = true
--- tfvars/provider.tf
+++ tfvars/provider.tf
@@ -7,11 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "remote-state-sureshbandaru"
-    key          = "multi-env-demo"
-    region       = "us-east-1"
-    use_lockfile = true
-    encrypt      = true
+
   }
 }
 
