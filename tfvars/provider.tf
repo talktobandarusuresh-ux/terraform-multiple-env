db069910026d46e6381d8abaa9f76bf61d110571
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-01-26 14:30:51 +0530

    Terraform

--- tfvars/provider.tf
+++ tfvars/provider.tf
@@ -1,13 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.16.0"
     }
   }
 
   backend "s3" {
-    
+    bucket       = "remote-state-sureshbandaru"
+    key          = "multi-env-demo"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
--- tfvars/variables.tf
+++ tfvars/variables.tf
@@ -1,44 +1,44 @@
 
-variable "project"{
-    default = "roboshop"
+variable "project" {
+  default = "roboshop"
 }
 
 variable "environment" {
-    type = string
+  type = string
 }
 
 variable "ami_id" {
-    type = string
-    default     = "ami-09c813fb71547fc4f"
+  type    = string
+  default = "ami-0220d79f3f480ecf5"
 }
 
 variable "instance_type" {
-    type = string
+  type = string
 }
 
 
 variable "cidr" {
-    type = list
-    default = ["0.0.0.0/0"]
+  type    = list(any)
+  default = ["0.0.0.0/0"]
 }
 
 variable "ingress_from_port" {
-    default = 0
+  default = 0
 }
 
 variable "ingress_to_port" {
-    default = 0
+  default = 0
 }
 
 variable "egress_from_port" {
-    default = 0
+  default = 0
 }
 
 variable "egress_to_port" {
-    default = 0
+  default = 0
 }
 
 variable "protocol" {
-    type = string
-    default = "-1"
+  type    = string
+  default = "-1"
 }
