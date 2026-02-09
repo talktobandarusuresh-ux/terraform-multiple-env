74a00963e224f7932585a2541a7e403aaf425311
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-02-09 18:46:01 +0530

    terraform

--- workspaces/provider.tf
+++ workspaces/provider.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.16.0"
     }
   }
 
   backend "s3" {
-    bucket = "remote-state-86s"
-    key    = "workspace-demo"
-    region = "us-east-1"
+    bucket       = "remote-state-bandaru"
+    key          = "workspace-demo"
+    region       = "us-east-1"
     use_lockfile = true
-    encrypt = true
+    encrypt      = true
   }
 }
 
